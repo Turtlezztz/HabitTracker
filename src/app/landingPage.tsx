08515f78fbad3c791abdacb79447bd7a7c271f52
Turtlezztz <ov@turtlezz.dk>
2025-02-08 16:23:18 +0100

    restructure landing page and apply global styles

---
+++ src/app/landingPage.tsx
@@ -0,0 +1,11 @@
+
+
+export default function LandingPage() {
+    return (
+        <>
+            <link rel="stylesheet" href="globals.css"/>
+            <h1 className="justify-self-center mt-32 font-bold text-5xl ">Habit tracker refined</h1>
+        </>
+    )
+}
+
