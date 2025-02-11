7f7e6b8c90743ee1d24f0e14ab24741ebefaea25
Turtlezztz <ov@turtlezz.dk>
2025-02-11 14:48:15 +0100

    add shadcn/ui integration

--- src/app/landingPage.tsx
+++ src/app/landingPage.tsx
@@ -4,7 +4,8 @@ export default function LandingPage() {
     return (
         <>
             <link rel="stylesheet" href="globals.css"/>
-            <h1 className="justify-self-center mt-32 font-bold text-5xl ">Habit tracker refined</h1>
+            <h1 className="justify-self-center mt-32 font-bold text-5xl">Habit tracker refined</h1>
+            <button>Get started</button>
         </>
     )
 }
